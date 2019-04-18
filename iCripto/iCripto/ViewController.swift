f09fcbf04fe7b3ba71c01a0b82babe4ba7480c8b
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-04-18 22:08:33 +0200

    Upload initial project

---
+++ iCripto/iCripto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCripto
+//
+//  Created by Sebastian Ortiz Velez on 18/04/2019.
+//  Copyright © 2019 Sebastian Ortiz Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
