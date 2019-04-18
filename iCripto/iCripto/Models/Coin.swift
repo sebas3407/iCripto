7468eea927a3a9af752f59b8c15933007e21a397
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-04-18 22:26:42 +0200

    Add Coin model and prepare main function to download data from the API

---
+++ iCripto/iCripto/Models/Coin.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct Coin : Codable {
+    let id, name, symbol, rank: String
+    let priceUsd, priceBtc, the24HVolumeUsd, marketCapUsd: String
+    let availableSupply, totalSupply, maxSupply, percentChange1H: String
+    let percentChange24H, percentChange7D, lastUpdated: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, symbol, rank
+        case priceUsd = "price_usd"
+        case priceBtc = "price_btc"
+        case the24HVolumeUsd = "24h_volume_usd"
+        case marketCapUsd = "market_cap_usd"
+        case availableSupply = "available_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case percentChange1H = "percent_change_1h"
+        case percentChange24H = "percent_change_24h"
+        case percentChange7D = "percent_change_7d"
+        case lastUpdated = "last_updated"
+    }
+}
--- iCripto/iCripto/ViewController.swift
+++ iCripto/iCripto/ViewController.swift
@@ -15,6 +15,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func DownloadQuote()
+    {
+        //Implementing URLSession
+        let urlString = "https://api.coinmarketcap.com/v1/ticker/?limit=0"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+            
+            guard let dataFromUrl = data else { return }
+            do {
+                let coins = try JSONDecoder().decode(Coin.self, from: dataFromUrl)
+                
+                DispatchQueue.main.async {
+                    
+                }
+                
+            } catch let jsonError {
+                print(jsonError)
+            }
+            
+            }.resume()
+        //End implementing URLSession
+    }
 }
 
