2ae3a6b4371a28bbc49254856d3f56d9291950fc
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-04-22 17:05:23 +0200

    Working on put data into a table

--- iCripto/iCripto/ViewController.swift
+++ iCripto/iCripto/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    var coins : Array<Coin> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,7 +31,7 @@ class ViewController: UIViewController {
             
             guard let dataFromUrl = data else { return }
             do {
-                let coins = try JSONDecoder().decode(Array<Coin>.self, from: dataFromUrl)
+                self.coins = try JSONDecoder().decode(Array<Coin>.self, from: dataFromUrl)
                 
                 DispatchQueue.main.async {
                 }
@@ -40,6 +42,18 @@ class ViewController: UIViewController {
             
             }.resume()
         //End implementing URLSession
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coins.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell=UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "mycell")
+        cell.textLabel?.text  = coins[indexPath.row].name
+        
+        return cell
     }
 }
 
