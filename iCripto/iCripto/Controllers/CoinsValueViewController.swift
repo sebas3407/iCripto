393fea6a3dabfe1dea27056eb89c4aeb27a9faf8
Sebastian Ortiz Velez <sebasortiz2000@gmail.com>
2019-10-22 16:44:46 +0200

    Add CoinsValueViewController

---
+++ iCripto/iCripto/Controllers/CoinsValueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CoinsValueViewController.swift
+//  iCripto
+//
+//  Created by Sebastian Ortiz Velez on 22/10/2019.
+//  Copyright © 2019 Sebastian Ortiz Velez. All rights reserved.
+//
+
+import UIKit
+
+class CoinsValueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
